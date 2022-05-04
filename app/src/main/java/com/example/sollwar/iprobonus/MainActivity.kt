a2490c5e321f19b430f6869304c0fd48659c99a8
SollWar <sollwarmorgrind@gmail.com>
2022-05-04 22:08:42 +0300

    Практически готово

--- app/src/main/java/com/example/sollwar/iprobonus/MainActivity.kt
+++ app/src/main/java/com/example/sollwar/iprobonus/MainActivity.kt
@@ -14,11 +14,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val idClient = "2c44d8c2-c89a-472e-aab3-9a8a29142315"
+        val idDevice = "7db72635-fd0a-46b9-81asdasd3b-1627e3aa02ea"
+        val scale: Float = 1f
+
         val isFragmentContainerEmpty = savedInstanceState == null
         if (isFragmentContainerEmpty) {
             supportFragmentManager
                 .beginTransaction()
-                .add(binding.fragmentContainer.id, BonusFragment.newInstance())
+                .add(
+                    binding.fragmentContainer.id, BonusFragment.newInstance(
+                        idClient,
+                        idDevice,
+                        scale
+                    )
+                )
                 .commit()
         }
     }
--- fragment_module/src/main/java/com/example/sollwar/fragment_module/BonusFragment.kt
+++ fragment_module/src/main/java/com/example/sollwar/fragment_module/BonusFragment.kt
@@ -1,45 +1,78 @@
 package com.example.sollwar.fragment_module
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.sollwar.fragment_module.databinding.FragmentBonusBinding
+import java.text.SimpleDateFormat
+
+const val ID_CLIENT = "ID_CLIENT"
+const val ID_DEVICE = "ID_DEVICE"
+const val SCALE = "SCALE"
 
 class BonusFragment : Fragment() {
 
     private var _binding: FragmentBonusBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModels<BonusViewModel>()
+    private var idClient = ""
+    private var idDevice = ""
+
+    private var scale: Float = 1f
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBonusBinding.inflate(inflater, container, false)
+        idClient = arguments?.getString(ID_CLIENT) as String
+        idDevice = arguments?.getString(ID_DEVICE) as String
+        scale = arguments?.getFloat(SCALE) as Float
+
+        binding.arrow.layoutParams.height = (binding.arrow.layoutParams.height * scale).toInt()
+        binding.arrow.layoutParams.width = (binding.arrow.layoutParams.width * scale).toInt()
+
+        binding.fire.layoutParams.height = (binding.fire.layoutParams.height * scale).toInt()
+        binding.fire.layoutParams.width = (binding.fire.layoutParams.width * scale).toInt()
+
+        binding.bonusCount.textSize = 24f * scale
+        binding.bonusFire.textSize = 16f * scale
+        binding.dataFire.textSize = 16f * scale
+
         return binding.root
     }
 
+    @SuppressLint("SimpleDateFormat")
     override fun onStart() {
         super.onStart()
-        viewModel.getUserBonus(
-            "2c44d8c2-c89a-472e-aab3-9a8a29142315",
-            "7db72635-fd0a-46b9-81asdasd3b-1627e3aa02ea"
-        )
+        viewModel.getUserBonus(idClient, idDevice)
         viewModel.userBonusLiveData.observe(
-            viewLifecycleOwner,
-            Observer { userBonus ->
-                userBonus.let {
-                    binding.bonusCount.text = userBonus.currentQuantity.toString()
-                    binding.bonusFire.text = userBonus.forBurningQuantity.toString()
-                    binding.dataFire.text = userBonus.dateBurning
-                    binding.bonusName.text = userBonus.typeBonusName
-                }
+            viewLifecycleOwner
+        ) { userBonus ->
+            userBonus.let {
+                binding.bonusView.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.INVISIBLE
+                val dateFormat =
+                    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(userBonus.dateBurning)
+                        ?.let { it1 ->
+                            SimpleDateFormat("dd.MM").format(
+                                it1
+                            )
+                        }
+                binding.bonusCount.text =
+                    getString(R.string.bonuses, userBonus.currentQuantity.toInt().toString())
+                binding.bonusFire.text =
+                    getString(R.string.bonuses, userBonus.forBurningQuantity.toInt().toString())
+                binding.dataFire.text = getString(R.string.date_burn, dateFormat)
             }
-        )
+        }
     }
 
     override fun onDestroy() {
@@ -48,6 +81,16 @@ class BonusFragment : Fragment() {
     }
 
     companion object {
-        fun newInstance() = BonusFragment()
+        fun newInstance(
+            idClient: String,
+            idDevice: String,
+            scale: Float
+        ) = BonusFragment().apply {
+            arguments = Bundle().apply {
+                putString(ID_CLIENT, idClient)
+                putString(ID_DEVICE, idDevice)
+                putFloat(SCALE, scale)
+            }
+        }
     }
 }
